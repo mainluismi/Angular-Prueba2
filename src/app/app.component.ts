ca705c520d443be0df3b59ad8965f6bb513786a3
mainluismi <luismisc02@gmail.com>
2024-01-26 12:25:34 +0100

    Creacion de la app de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user/user.component';
+import { GamesComponent } from './games/games.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, UserComponent, GamesComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-prueba2';
+  city = 'madrid';
 }
